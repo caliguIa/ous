5f5aa12455bf3737c01a37a55fc83a5601fa7bfb
Cal <accounts@cal.rip>
2024-01-21 19:42:14 +0000

    feat: added in demo fetch error toggle

--- src/components/commissionCalculator/currencyCalcWrapper.tsx
+++ src/components/commissionCalculator/currencyCalcWrapper.tsx
@@ -2,9 +2,16 @@ import { useCommissionRates } from '../../hooks';
 import { CommissionCalcInner } from './currencyCalcInner';
 import { LANG } from '../../utils';
 import Widget from '../widget';
+import { FC } from 'react';
 
-export const CommissionCalcWrapper = () => {
-  const { isLoading, isError } = useCommissionRates({ willError: false });
+type Props = {
+  willError: boolean;
+};
+
+export const CommissionCalcWrapper: FC<Props> = ({ willError }: Props) => {
+  // if willError is true, the hook will fail to fetch data
+  // the fetch will re-run with an exponential backoff and eventually fail, only added for demo purposes
+  const { isLoading, isError } = useCommissionRates({ willError });
 
   return (
     <Widget isLoading={isLoading} isError={isError} title={LANG['EN']['WIDGET.TITLE']}>
--- src/components/widget/widget.tsx
+++ src/components/widget/widget.tsx
@@ -12,15 +12,15 @@ export const Widget: FC<Props> = ({ children, isLoading, isError, title }) => {
   // TODO:
   // - context for current language
   // - current lang switcher
-  // - make fetch error toggle
 
   return (
-    <div className="bg-gray-100 h-screen w-screen">
+    <div className="bg-gray-100 h-auto w-screen">
       <div className="bg-white h-auto w-100 p-4 m-5 rounded-md border border-gray-300">
         <h3 className="text-black text-lg font-black mb-5">{title}</h3>
         <div className="min-h-64">
-          {isLoading ? <div>{LANG['EN']['WIDGET.LOADING']}</div> : children}
-          {isError && !isLoading && <div>{LANG['EN']['WIDGET.ERROR_RATES']}</div>}
+          {isLoading && <div>{LANG['EN']['WIDGET.LOADING']}</div>}
+          {isError && <div>{LANG['EN']['WIDGET.ERROR_RATES']}</div>}
+          {!isLoading && !isError && children}
         </div>
       </div>
     </div>
